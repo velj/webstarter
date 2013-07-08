90da77d8d2349164f5c429c81372e85d10b142b6
Eric <eric@eric-VirtualBox.(none)>
2013-07-08 15:58:01 -0500

    updatd to use index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+
 
 var app = express.createServer(express.logger());
+var buf = new Buffer(256,"utf-8");
+buf = fs.readFileSync("index.html","utf-8")
+
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!!');
+  response.send(buf.toString('utf8', 0, buf.length));
 });
 
 var port = process.env.PORT || 5000;
